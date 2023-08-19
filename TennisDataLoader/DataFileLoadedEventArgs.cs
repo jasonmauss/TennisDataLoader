54e59f849792a2ab0b01afb56adc0c7308535ee1
Jason Mauss <jason.mauss@gmail.com>
2023-08-18 23:43:38 -0700

    Adding some event args for what I think I will want to provide to the events later

---
+++ TennisDataLoader/DataFileLoadedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisDataLoader
+{
+    /// <summary>
+    /// Custom event args class for when a data file has been loaded into the database
+    /// </summary>
+    public class DataFileLoadedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The total numbers of rows processed from the data file
+        /// </summary>
+        public int TotalRowsProcessed { get; set;}
+
+        // how long it took to process all the rows
+        public string TimeElapsed { get; set; }
+
+        /// <summary>
+        /// Constructor method
+        /// </summary>
+        /// <param name="totalRowsProcessed"></param>
+        /// <param name="timeElapsed"></param>
+        public DataFileLoadedEventArgs(int totalRowsProcessed, string timeElapsed)
+        {
+            TotalRowsProcessed = totalRowsProcessed;
+            TimeElapsed = timeElapsed;
+        }
+    }
+}
